d95cad9ccb7987323cfad144e0e5a89eb7d4ff8b
YOGESHWARAN K <yogeshwaranky2103@gmail.com>
2024-12-20 21:11:39 +0530

    cateogry

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { Suspense } from "react";
 import { AppProviders } from "./providers";
 import Applayout from "./Components/applayout/Applayout.component";
